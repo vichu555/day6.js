13ed6ec07de8bf5fa2458d38b4354cd1eca9da7c
vichu555 <80614368+vichu555@users.noreply.github.com>
2024-04-29 11:03:54 +0530

    Add files via upload

---
+++ day6.js
@@ -0,0 +1,13 @@
+//math and math methods
+let m=64;
+let n=-9;
+let c=9.679;
+console.log(Math.sqrt(m))
+console.log(Math.abs(n))
+console.log(Math.PI)
+console.log(Math.random()*20+1)
+console.log(Math.round(c))
+console.log(Math.ceil(c))
+console.log(Math.floor(c))
+console.log(Math.trunc(c))
+console.log(c.toFixed(2))
